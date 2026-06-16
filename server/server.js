2bbcb11572b753c540c09856601934da13cac6ed
Mohamed Sabeek <safeesafee0000@gmail.com>
2026-06-16 12:11:11 +0530

    chore: ignore video files and update server

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
